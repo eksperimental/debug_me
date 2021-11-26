422239aaa77d5dcc96fa9dfe986af7e005146079
Eksperimental <eksperimental@autistici.org>
2021-11-26 13:37:47 -0500

    Test exceptions

--- test/fn_test.exs
+++ test/fn_test.exs
@@ -4,15 +4,28 @@ defmodule FnTest do
 
   # https://elixirforum.com/t/weird-errors-in-non-matching-clause-in-anonymous-function/44110
 
-  test "f1" do
-    assert Fn.f1(& &1, :foo) == :foo
+  # test "f1" do
+  #   assert Fn.f1(& &1, :foo) == :foo
+  # end
+
+  test "f1 exception" do
+    assert_raise CaseClauseError, fn ->
+      Fn.f1(& &1, :foo)
+    end
   end
 
-  test "f2" do
-    assert Fn.f2(:foo) == :foo
+  # test "f2" do
+  #   assert Fn.f2(:foo) == :foo
+  # end
+
+  test "f2 exception" do
+    assert_raise FunctionClauseError, fn ->
+      Fn.f2(:foo)
+    end
   end
 
   test "f3" do
     Fn.f3(& &1, :foo)
   end
+
 end
