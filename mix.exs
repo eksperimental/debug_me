4e99966eb7a183dcbf7850d760ec9126c4e46716
Eksperimental <eksperimental@autistici.org>
2021-11-26 12:48:18 -0500

    Add Elixir Workflow matrix of all available Elixir/OTP versions

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule DebugMe.MixProject do
     [
       app: :debug_me,
       version: "0.1.0",
-      elixir: "~> 1.13-rc",
+      elixir: "~> 1.0",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
